f72e715debd1625b4c7e33ba001a88cc76bd2fb8
Yanislav <historicalsection83@proton.me>
2025-09-09 23:27:02 +0100

    added wc command

---
+++ wc/README.txt
@@ -0,0 +1,38 @@
+# Wc Clone
+
+My attempt at recreating the `wc` command from Linux.
+
+## Features
+- Print the lines, words, and characters contained in a file
+- Use -l -w -c flags to print only the lines, words, or characters contained in a file
+- Use a combination of the flags to print the respective statistics of a file
+
+Example:
+~/c-code/linux-commands/wc/main ~/recipes/chocolate-cake.txt
+Output:
+14  25  184
+
+Example:
+~/c-code/linux-commands/wc/main -lc ~/recipes/chocolate-cake.txt
+Output:
+14  184
+
+-l will print the amount of lines contained in the file
+-w will print the amount of words contained in the file
+-lw will print the amount of lines and words contained in the file
+
+Feel free to compare the output of my version against the real `wc` command to check if the output is accurate. If there are cases where it doesn't work correctly, please let me know!
+
+## Installation
+No installation required.
+
+Just download the 'main' file if you're on Linux and run it directly as outlined above.
+
+Or if you are not on a Linux computer, download the 'main.c' file and compile it using a compiler of your choice, then run the output file as outlined above.
+
+## Notes
+It's worth noting that the real `wc` Linux command is capable of taking the argument of the -l -w -c flags and the input file in any order. Example (both of these work):
+~/main -l ~/recipes/chocolate-cake.txt
+~/main ~/recipes/chocolate-cake.txt -l
+
+My implementation does not yet include this functionality.
---
+++ wc/main.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+    char* flags = "";
+    FILE* pFile;
+    char buffer[1024] = {0};
+
+    int i;
+    int lines = 0;
+    int words = 0;
+    int chars = 0;
+    int inside_word = 0;
+    char previous_char = ' ';
+
+    if (argc == 2)
+    {
+        pFile = fopen(argv[1], "r");
+
+        if (pFile == NULL)
+        {
+            printf("Couldn't open file\n");
+            return 1;
+        }
+    }
+    else
+    {
+        flags = argv[1];
+        pFile = fopen(argv[2], "r");
+
+        if (pFile == NULL)
+        {
+            printf("Couldn't open file\n");
+            return 1;
+        }
+    }
+
+    while (fgets(buffer, sizeof(buffer), pFile))
+    {
+        for (i = 0; i < sizeof(buffer) && (buffer[i] != '\0'); ++i)
+        {
+            ++chars;
+            if (buffer[i] == '\n') ++lines;
+            
+            if (buffer[i] != ' ' && buffer[i] != '\n' && !inside_word)
+            {
+                inside_word = 1;
+                ++words;
+            }
+            else if ((buffer[i] == ' ' || buffer[i] == '\n') && inside_word)
+            {
+                inside_word = 0;
+            }
+
+            // neccessary to keep track of trailing \n character at end of file
+            previous_char = buffer[i];
+        }
+    }
+
+    // if last char in the file isn't \n, count an extra line
+    if (previous_char != '\n') ++lines;
+
+    int j = 0;
+    while (flags[j] != '\0')
+    {
+        if (flags[j] == 'l') printf("%d  ", lines);
+        else if (flags[j] == 'w') printf("%d  ", words);
+        else if (flags[j] == 'c') printf("%d  ", chars);
+        ++j;
+    }
+
+    if (j == 0) printf("%d  %d  %d", lines, words, chars);
+    putchar('\n');
+
+    fclose(pFile);
+    
+    return 0;
+}
